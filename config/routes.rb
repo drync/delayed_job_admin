ef39c0d3d03cc12d6a5e9bc8d9222b6d262adb12
Matt Griffin <matt@griffinonline.org>
2018-06-27 11:54:15 -0400

    Update routes for Rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  match 'delayed_job_admin' => 'delayed_job_admin#index', :as => 'delayed_job_admin'
-  match 'delayed_job_admin/restart' => 'delayed_job_admin#restart', :as => 'delayed_job_admin/restart'
-  match 'delayed_job_admin/check_status' => 'delayed_job_admin#check_status', :as => 'delayed_job_admin/check_status'
+  get 'delayed_job_admin', to: 'delayed_job_admin#index', as: 'delayed_job_admin'
+  get 'delayed_job_admin/restart', to: 'delayed_job_admin#restart', as: 'delayed_job_admin/restart'
+  get 'delayed_job_admin/check_status', to: 'delayed_job_admin#check_status', as: 'delayed_job_admin/check_status'
 
-  delete 'delayed_job_admin/:job_id' => 'delayed_job_admin#destroy', :as => 'delete_job_delayed_job_admin'
-  match 'delayed_job_admin/retry' => 'delayed_job_admin#retry_job', :as => 'retry_job_delayed_job_admin', :method => :put
-  match 'delayed_job_admin/run_now' => 'delayed_job_admin#run_job_now', :as => 'run_job_now_delayed_job_admin', :method => :put
+  delete 'delayed_job_admin/:job_id', to: 'delayed_job_admin#destroy', as: 'delete_job_delayed_job_admin'
+  put 'delayed_job_admin/retry', to: 'delayed_job_admin#retry_job', as: 'retry_job_delayed_job_admin'
+  put 'delayed_job_admin/run_now', to: 'delayed_job_admin#run_job_now', as: 'run_job_now_delayed_job_admin'
 end
